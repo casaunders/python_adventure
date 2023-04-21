d823089c2136f49f2d9220e24c8284be8977e324
Christopher Saunders <christopherallansaunders@gmail.com>
2023-04-20 21:29:45 -0500

    Initial commit.

---
+++ python_adventure.py
@@ -0,0 +1,7 @@
+# Project needs functions, if/else statements and logging.
+# Create functions to process events to work through game, generate ASCII art etc.
+# Story will progress by selecting the appropriate choices.
+# Bring in ASCII art from separate files.
+
+
+
